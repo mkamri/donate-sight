fd59d5b8ee9a020ebcc889840ff0e5c9799509d1
mkamri <mail@mikamarie.com>
2020-07-03 17:54:05 -0400

    Simplify copyright function

--- functions.php
+++ functions.php
@@ -138,25 +138,9 @@ GET DATE FOR FOOTER
 ************************************************************************/
 
 function comicpress_copyright() {
-    global $wpdb;
-    $copyright_dates = $wpdb->get_results("
-    SELECT
-    YEAR(min(post_date_gmt)) AS firstdate,
-    YEAR(max(post_date_gmt)) AS lastdate
-    FROM
-    $wpdb->posts
-    WHERE
-    post_status = 'publish'
-    ");
-    $output = '';
-    if($copyright_dates) {
-    $copyright = "&copy; " . $copyright_dates[0]->firstdate;
-    if($copyright_dates[0]->firstdate != $copyright_dates[0]->lastdate) {
-    $copyright .= '-' . $copyright_dates[0]->lastdate;
-    }
-    $output = $copyright;
-    }
-    return $output;
-    }
-    
+    $years = array( 2019, date( 'Y' ) );
+    $years = array_unique( array_filter( $years ) );
+    $copyright = implode(' - ', $years);
+    return '©' . $copyright;
+}
     ?>
